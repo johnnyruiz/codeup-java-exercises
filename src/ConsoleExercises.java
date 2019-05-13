f38ea1145ca2cc4c467307b6a68473b54ff69618
johnnyruiz <ruiz.johnny.p@gmail.com>
2019-05-13 13:40:45 -0500

    pushing up work

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -1,12 +1,21 @@
-import java.util.Scanner;
+//import java.util.Scanner;
+import java.text.*;
+
 
 
 public class ConsoleExercises {
 
-    public static void main(String[] args) {
-        double pi = 3.14159;
-        float precisePi = (float) pi;
-        System.out.format("the value of pi is approximately: %s", precisePi);
+    public static void customFormat(String pattern, double value) {
+        {
+            DecimalFormat myFormatter = new DecimalFormat(pattern);
+            String output = myFormatter.format(value);
+            System.out.println(value + " " + pattern + " " output);
+        }
+        static public void main (String[] args){
+            customFormat();
+            double pi = 3.14159;
+            System.out.format("the value of pi is approximately: %s", pi)
+        }
     }
 
 }
