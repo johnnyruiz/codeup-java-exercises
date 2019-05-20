a5ee8f78b5c65ba0dc13891c16f9027abd88df9f
johnnyruiz <ruiz.johnny.p@gmail.com>
2019-05-20 10:06:46 -0500

    util exercises added methods and method overloading

--- src/util.java
+++ src/util.java
@@ -1,7 +1,34 @@
+import java.util.Scanner;
+
 public class util {
 
-    private class Input{
-        private String scanner;
+    private Scanner scanner = new Scanner(System.in);
 
+    //would be useful for user choosing attack in java project
+    public String getString(String msg){
+        System.out.println(msg);
+        return scanner.nextLine().trim().toLowerCase();
     }
+    public String getString(){
+        return getString("Write something: ");
+    }
+
+    public boolean yesNo(){
+        getString("Type yes/no");
+        return (getString().equals("yes"));
+    }
+
+    public int getInt(){
+        return Integer.parseInt((getString("Give me a number: ")));
+    }
+
+    public int getInt(int max, int min){
+        int num = getInt();
+
+        if(num < min || num > max){
+            return getInt();
+        }
+        return num;
+    }
+
 }
